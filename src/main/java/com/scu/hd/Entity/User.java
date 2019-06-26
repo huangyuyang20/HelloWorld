9ee3bd16c9a3b1b14c4bebd629abb2939a04ce07
黄宇阳 <2439735732@qq.com>
2019-06-26 17:24:04 +0800

    HelloWorld

---
+++ src/main/java/com/scu/hd/Entity/User.java
@@ -0,0 +1,49 @@
+package com.scu.hd.Entity;
+
+public class User {
+    private String userId;
+    private String userPassword;
+    private String userRole;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
+    public User(String userId, String userPassword, String userRole) {
+        this.userId = userId;
+        this.userPassword = userPassword;
+        this.userRole = userRole;
+    }
+
+    public User() {
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", userPassword='" + userPassword + '\'' +
+                ", userRole='" + userRole + '\'' +
+                '}';
+    }
+}
