762620daf79635813bbcfeffc199187723a22bac
wbx <wbx123450@163.com>
2019-06-29 21:35:21 +0700

    Merge remote-tracking branch 'remotes/origin/zsz' into wbx_branch
    # Conflicts:
    #	src/main/java/com/scu/hd/entity/StudApplyInformation.java

--- src/main/java/com/scu/hd/dao/LoginMapper.java
+++ src/main/java/com/scu/hd/dao/LoginMapper.java
@@ -19,24 +19,22 @@ public interface LoginMapper {
 
     public List<StudApplyInformation> selectStudApplyInformation();
 
-    @Select("select * from stu_apply_information")
+
     public List<StudApplyInformation> manager1GetInformation();
 
-    @Select("select * from stu_apply_information where stu_level1manager = 1")
+
     public List<StudApplyInformation> manager2GetInformation();
 
-    @Select("select * from stu_apply_information where stu_level2manager = 1")
+
     public List<StudApplyInformation> manager3GetInformation();
 
-    //所有通过的
-    @Select("select * from stu_apply_information where stu_level3manager = 1")
+
     public List<StudApplyInformation> studentGetInformation();
 
-    //用户自己的
-    @Select("select * from stu_apply_information where user_id = #{userId}")
+
     public StudApplyInformation getMyApplyInformation(String userId);
 
-    @Select("select * from stu_apply_information where stu_selfcheck = 1")
+
     public List<StudApplyInformation> finalOutcome();
 
 
