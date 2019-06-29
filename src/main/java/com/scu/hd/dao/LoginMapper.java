7890d1c4816b45e3df970a3122170b6b7831f9a8
wbx <wbx123450@163.com>
2019-06-29 15:09:45 +0700

    Merge remote-tracking branch 'remotes/origin/master' into wbx_branch

--- src/main/java/com/scu/hd/dao/LoginMapper.java
+++ src/main/java/com/scu/hd/dao/LoginMapper.java
@@ -1,7 +1,6 @@
 package com.scu.hd.dao;
 
 import com.scu.hd.entity.StudApplyInformation;
-import com.scu.hd.entity.Student;
 import com.scu.hd.entity.User;
 import org.springframework.stereotype.Repository;
 
